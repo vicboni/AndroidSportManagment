5f771aaf7523b4d0c20787d8af0777ba63c69f69
vicbo <vbonifacio50@gmail.com>
2018-04-04 10:26:48 +0200

    first commit

---
+++ app/src/main/java/navfrag/myfragapp/com/navigationdrawersample/Gallery.java
@@ -0,0 +1,29 @@
+package navfrag.myfragapp.com.navigationdrawersample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by vicbo on 04/04/2018.
+ */
+
+public class Gallery extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.gmaps, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        getActivity().setTitle("Gallery");
+
+    }
+}
